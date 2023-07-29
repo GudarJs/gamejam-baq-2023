59a02aa56545ead77ecc4b8944c87f20c2c5df67
Dario Guzman <guzman.d.dario@gmail.com>
2023-07-28 22:10:13 -0700

    Add test character and test background

---
+++ lib/actors/lya.dart
@@ -0,0 +1,31 @@
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:gamejam_baq_2023/main.dart';
+
+import '../world/ground.dart';
+
+class Lya extends SpriteComponent with CollisionCallbacks, HasGameRef<GameJam2023> {
+
+  Lya() : super() {
+    debugMode = true;
+  }
+
+  bool onGround = false;
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    
+    add(RectangleHitbox());
+  }
+
+  @override
+  void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+    super.onCollision(intersectionPoints, other);
+
+    if (other is Ground) {
+      gameRef.velocity.y = 0;
+      onGround = true;
+    }
+  }
+}
